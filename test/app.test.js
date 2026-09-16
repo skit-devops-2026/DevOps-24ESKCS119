a6f39241c98c53ac4e80beda7fd10d2102a4c627
Divyanshu <b240899@skit.ac.in>
2026-09-16 16:17:39 +0530

    Feat: Add CI pipeline and Jenkinsfile for M3 and M4

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Basic Application Tests', () => {
+    it('should pass a simple health check', () => {
+        assert.strictEqual(1 + 1, 2);
+    });
+});
